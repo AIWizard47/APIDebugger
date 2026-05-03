1cf8ec0045e5cb9766364e742c8d556aaec96a1a
Sam <sambhavvinayak16@gmail.com>
2026-05-03 13:42:10 +0530

    new update with new task for all of you contributors

--- include/http/http.h
+++ include/http/http.h
@@ -2,6 +2,7 @@
 #define HTTP_H
 
 #include <string>
+#include <map>
 #include <winsock2.h>
 #include <openssl/ssl.h>
 
@@ -11,6 +12,14 @@ struct URL {
     int port;
     bool is_https;
 };
+
+// This is in development part so any one can use this to modify the code and make it better. I am open to any suggestion and contribution. I will be very happy to see your contribution in this project. I am also open to any suggestion and feedback. I will be very happy to see your contribution in this project. I am also open to any suggestion and feedback. I will be very happy to see your contribution in this project. I am also open to any suggestion and feedback. I will be very happy to see your contribution in this project. I am also open to any suggestion and feedback. I will be very happy to see your contribution in this project. I am also open to any suggestion and feedback. I will be very happy to see your contribution in this project. I am also open to any suggestion and feedback. I will be very happy to see your contribution in this project. I am also open to any suggestion and feedback. I will be very happy to see your contribution in this project. I am also open to any suggestion and feedback. I will be very happy to see your contribution in this project. I am also open to any suggestion and feedback. I will be very happy to see your contribution in this project. I am also open to any suggestion and feedback. I will be very happy to see your contribution in this project. I am also open to any suggestion and feedback. I will be very happy to see your contribution in this project. I am also open to any suggestion and feedback. I will be very happy to see your contribution in this project. I am also open to any suggestion and feedback. I will be very happy to see your contribution in this project. I am also open to any suggestion and feedback.
+// struct HttpResponse
+// {
+//     int statusCode;
+//     std::map<std::string,std::string> headers;
+//     std::string body;
+// };
 
 class HTTPClient {
 public:
@@ -37,6 +46,7 @@ private:
     // I/O helpers (unify HTTP + HTTPS)
     int writeAll(SOCKET sock, SSL* ssl, const char* data, int len);
     int readSome(SOCKET sock, SSL* ssl, char* buf, int cap);
+
 };
 
 #endif
