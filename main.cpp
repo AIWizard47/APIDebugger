f6bb9191f98d15743c945a3eadd0ff7697bb2f31
Sam <sambhavvinayak16@gmail.com>
2026-05-03 14:08:36 +0530

    Time changes in m/s

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "http\http.h"
+#include <chrono>
 #include <iostream>
 
 int main(int argc, char* argv[]) {
@@ -16,19 +17,30 @@ int main(int argc, char* argv[]) {
         }
         std::cout<<"Number of command : "<<argc<<"\n";
         std::cout << "---- HTTPS ----\n";
-        std::cout << c.GET(url) << "\n";
+        auto start = std::chrono::high_resolution_clock::now(); // Get current time point
+        // ... code to measure ...
+        std::string res = c.GET(url);
+        auto end = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double, std::milli> elapsed = end - start; // Difference
+        std::cout << res << "\n";
+        std::cout << "Waited: " << elapsed.count() << "ms\n";
+
 
         std::cout << "---- POST HTTPS ----\n";
-                std::string json =
+        std::string json =
             R"({"title":"Fuck","body":"you","userId":1})";
-
+        start = std::chrono::high_resolution_clock::now(); // Get current time point
+        // ... code to measure ...
         std::string response =
             c.POST(
                 url,
                 json
             );
-
+        end = std::chrono::high_resolution_clock::now();
+        elapsed = end - start; // Difference
         std::cout << response << std::endl;
+        std::cout << "Waited: " << elapsed.count() << "ms\n";
+
     }
     catch (const std::exception& e) {
         std::cout<<"Error: ";
